b51e01c6bc7810ede53a505eaf96f5e0ff7262e3
nadia <nadiaikon@gmail.com>
2019-08-28 21:22:32 -0400

    my-blog commit

---
+++ src/build/precache-manifest.d48c9e37df160b9cc8a09df6d7240f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fef9e96ea71d322c6dbd72ed5162984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb870242542748ca1309",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "3d5be05218d4b6b1f4f2",
+    "url": "/static/js/2.bc5c0032.chunk.js"
+  },
+  {
+    "revision": "eb870242542748ca1309",
+    "url": "/static/js/main.0c7c4919.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
